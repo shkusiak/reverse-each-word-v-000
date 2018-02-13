c939f515b2b49019b825da6517555796d5963870
Shea Kusiak <shkusiak@gmail.com>
2018-02-13 03:16:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,8 @@
 def reverse_each_word(string)
   #array = string.reverse().split(" ").reverse().join(" ")
 
-#string.split(" ").collect.join(" ") {|word|
-#  word.reverse().split(" ").reverse().join()
-#}
-
   string.split(" ").collect {|word|
     word.reverse().split(" ").reverse().join()
   }.join(" ")
-
-
-  #array.collect. {|word|
-  #  word.reverse().split(" ").reverse().join()
-  #}
-
 
 end
