ecc2f6146ee025f7dc2ba5a4ad2b494bf9af71a1
Shea Kusiak <shkusiak@gmail.com>
2018-02-13 03:16:09 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,8 @@
 def reverse_each_word(string)
   #array = string.reverse().split(" ").reverse().join(" ")
 
-#string.split(" ").collect.join(" ") {|word|
-#  word.reverse().split(" ").reverse().join()
-#}
-
-  array = string.split(" ").collect {|word|
+  string.split(" ").collect {|word|
     word.reverse().split(" ").reverse().join()
-  }
-
-  array.join(" ")
-  #array.collect. {|word|
-  #  word.reverse().split(" ").reverse().join()
-  #}
-
+  }.join(" ")
 
 end
